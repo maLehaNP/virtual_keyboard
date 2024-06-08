b22e5dfd999b7e3b1b6b62ebc07f164d30ffc374
maLeha <maLehaNP@users.noreply.github.com>
2024-06-08 20:40:28 +0400

    2

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ class Widg(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.init_ui()
 
-    def init_ui(self):
         self.setMouseTracking(True)
 
     def mouseDoubleClickEvent(self, event):
